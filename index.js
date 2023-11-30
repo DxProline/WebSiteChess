758e56515599508686a35ce06e81bc8cb3416511
DxProline <jendad185@gmail.com>
2023-11-30 12:14:57 +0100

    taaak krysy 12:14

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function checkEmail(mailElement) {
     if (mailElement.value.includes ("@")){
         // TRUE
         mailElement.style.border = "2px solid green"
+        disableMessageEmailProblem();
     }else{
         // FALSE
         mailElement.style.border = "2px solid red"
+        showMessageEmailProblem();
         }
     
 
@@ -13,22 +15,28 @@ function checkNumber(numberElement) {
     if (numberElement.value.match(/^\d{9}$/)) {
         // TRUE
         numberElement.style.border = "2px solid green";
-        disableMessageEmailProblem();
+
     } else {
         // FALSE
         numberElement.style.border = "2px solid red";
-        ShowMessageEmailProblem();
     
     }
 }
 
-function ShowMessageEmailProblem(){
+function showMessageEmailProblem() {
     let pElement = document.createElement('p');
     pElement.classList.add("red");
     pElement.innerText = "V e-mailu chybí zavináč!"
-    if(document.querySelector("p.red")){
+
+    if(!document.querySelector("p.red")){
         const formElement = document.querySelector("form")
         formElement.insertAdjacentElement("afterend",pElement);
     }
 
+}
+
+function disableMessageEmailProblem(){
+    if(document.querySelector("p.red")){
+        document.querySelector("p.red").remove();
+    }
 }
