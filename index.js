249916b4746425b0a7e1e85dddde3e362cc5ebee
DxProline <jendad185@gmail.com>
2023-11-30 11:46:06 +0100

    11:45

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ function checkNumber(numberElement) {
     if (numberElement.value.match(/^\d{9}$/)) {
         // TRUE
         numberElement.style.border = "2px solid green";
+        disableMessageEmailProblem();
     } else {
         // FALSE
         numberElement.style.border = "2px solid red";
+        ShowMessageEmailProblem();
+    
     }
 }
+
+function ShowMessageEmailProblem(){
+    let pElement = docunebt.createElement('p');
+    pElement.classList.add("red");
+    pElement.innerText = "V e-mailu chybí zavináč!"
+
+    
+}
+
+const formElement = document.querySelector("")
